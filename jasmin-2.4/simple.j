541ca6d25acad9165a54dd805a903db03c337bdc
Hagar Usama <Hagar_Usama@outlook.com>
2020-05-22 06:48:14 +0200

    * add jasmine file * detect problem: Diff Java and JDK

---
+++ jasmin-2.4/simple.j
@@ -0,0 +1,12 @@
+iconst_5
+istore_1
+iload_1
+iload_1
+iconst_2
+imul
+iadd
+istore_2
+getstatic     #2
+iload_2
+invokevirtual #3
+return
--- java folder/Test.java
+++ java folder/Test.java
@@ -1,29 +1,14 @@
 
 public class Test{
     public static void main(String[] args){
-        //int y;
-        //y=5;
-        //int x;
-        //x = y+y*5;
-        /*
-        int i;
-        for(i=0; i<100; i++){
-
-
-        }
-
-        */
-
+       
         int y;
         y =5;
         int x;
         x = y+ y*2;
-  /*       float i;
-        for(i=0; i<100; i++){
 
+        System.out.print(x);
 
-        }
- */
     }
  
   
